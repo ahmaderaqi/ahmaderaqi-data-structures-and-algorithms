5d7fe52308639ab39ea64a5bc2dddd62be11ddc8
ahmad zaher <118004544+ahmaderaqi@users.noreply.github.com>
2023-05-02 17:22:34 +0300

    Create test_cc_5.py

---
+++ code-challenges/linked-list/tests/test_cc_5.py
@@ -0,0 +1,21 @@
+import pytest
+from linked_list import Linked_list
+# Test empty linked list
+def test_empty_ll():
+    ll = Linked_list()
+    expected = "Empty LinkeList"
+    actual = str(ll)
+    assert expected == actual
+
+# # Test appending 3 nodes
+def test_append_three_nodes(ll):
+    expected = "3 --> Ahmad --> 10 -->  None"
+    actual = str(ll)
+    assert expected == actual
+@pytest.fixture
+def ll():
+    ll = Linked_list()
+    ll.insert(10)
+    ll.insert("Ahmad")
+    ll.insert(3)
+    return ll
