2fac10819b719fcfba09ca9b6ca207466ecb6593
ahmaderaqi <ahmadzaher573@gmail.com>
2023-05-14 18:10:56 +0000

    lastUpdate

--- code_challenges/stack_and_queue/stack_queue_animal_shelter.py
+++ code_challenges/stack_and_queue/stack_queue_animal_shelter.py
@@ -1,3 +1,4 @@
+from node import Node
 class Animal:
     def __init__(self, name, species):
         self.name = name
@@ -10,45 +11,60 @@ class Animal:
 
 class  AnimalShelter():
     def __init__(self):
-        
-        
-       
-        self.cat_queue=[]
-        self.dog_queue=[]
-        self.container=[]
-        self.container1=[]
+        self.front = None
+        self.rear = None
 
     def enqueue(self,animal):
-      if animal.species == "Cat":
-            self.cat_queue.append(animal.name)
-           
-            return self.cat_queue
+        node=Node(animal)
+        type=node.value.species
+        name=node.value.name
+        # print(node.value)
+        # print(type)
 
+        #if the queue is empty
+        if not self.rear:
+            self.front = node
+            self.rear = node
+        else:
+            self.rear.next = node
+            self.rear = node
         
-      elif animal.species == "Dog":
-            self.dog_queue.append(animal.name)
-            return self.dog_queue
 
         
     def dequeue(self, pref):
-         if pref=="Cat":
-            #  if len(self.cat_queue)==0:
-            #     return "I Have no cats"
+         #if the queue is empty
+        if self.front == None:
+            return "This queue is empty"
+        if self.front == self.rear:
+            self.rear = None
+            #self.front = None
+            if self.front.value.species==pref:
+                temp = self.front
+                self.front = self.front.next
+                temp.next = None
+                return temp.value
+            else:
+                return f"there's no {pref} in our queue"
+        # print(self.front.value.name)
+        temp1 =self.front
+        temp2=self.front.next
+        # print(temp1.value.name)
+        # print(not temp2==None)
+        # first node(front)
+        if self.front.value.species==pref:
+            self.front=self.front.next
+            return temp1.value.name
+        while temp2 is not None:
+            # print("hello")
+            if temp2.value.species==pref:
+                temp1.next=temp2.next
+                temp2.next=None
+                # print("hi")
+                return temp2.value.name
+            temp2=temp2.next
             
-             while len(self.cat_queue)!=0:
-                self.container.append(self.cat_queue.pop())
-           
-             return self.container.pop()
-    
-         elif pref=="Dog":
-            #  if len(self.dog_queue)==0:
-            #     return "I Have no Dogs"
-            
-             while len(self.dog_queue)!=0:
-                self.container1.append(self.dog_queue.pop())
-            #  self.cat_queue= self.container1
-            
-             return self.container1.pop()
+        return f"there's no {pref} in our queue"
+
          
     # def __str__(self):
     #     cat_queue_str = ', '.join([str(animal) for animal in self.container])
@@ -65,18 +81,20 @@ shelter = AnimalShelter()
 
 cat1 = Animal("Whiskers_cat", "Cat")
 (shelter.enqueue(cat1))
+(shelter.enqueue(Animal('dog1','Dog')))
+
 (shelter.enqueue(Animal('Fluffy_cat','Cat')))
-(shelter.enqueue(Animal('yuio_cat','Cat')))
-print(shelter.dequeue('Cat'))
-print(shelter.dequeue('Cat'))
-print(shelter.dequeue('Cat'))
-
-# # # print(shelter.dequeue('Dog'))
-(shelter.enqueue(Animal('Rex_dog', 'Dog')))
-(shelter.enqueue(Animal('Rex2_cat', 'Dog')))
-
+(shelter.enqueue(Animal('dog2','Dog')))
 print(shelter.dequeue('Dog'))
-print(shelter.dequeue('Dog'))
+# print(shelter.dequeue('Cat'))
+# print(shelter.dequeue('Cat'))
+
+# # # # print(shelter.dequeue('Dog'))
+# (shelter.enqueue(Animal('Rex_dog', 'Dog')))
+# (shelter.enqueue(Animal('Rex2_cat', 'Dog')))
+
+# print(shelter.dequeue('Dog'))
+# print(shelter.dequeue('Dog'))
 # print(shelter.dequeue('Dog'))
 # shelter.enqueue(Animal('Rex', 'Dog'))
 
