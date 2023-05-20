116af3b2f48ac3900d51c9dd9a3e5656bad69d61
ahmaderaqi <ahmadzaher573@gmail.com>
2023-05-20 15:05:18 +0000

    valid

--- code_challenges/stack_and_queue/stack_queue_brackets_CC13_folder/stack_queue_brackets_file.py
+++ code_challenges/stack_and_queue/stack_queue_brackets_CC13_folder/stack_queue_brackets_file.py
@@ -1,68 +1,87 @@
 from code_challenges.stack_and_queue.stack_file import Stack
 from code_challenges.stack_and_queue.node import Node
+from queue import Queue
+from collections import deque
+def is_balanced_parenthesis(sequence):
+    stack = []
+    queue = deque()
+
+    for char in sequence:
+        if char == '(':
+            stack.append(char)
+            queue.append(char)
+        elif char == ')':
+            if not stack or stack[-1] != '(':
+                return False
+            stack.pop()
+            queue.popleft()
+
+    return len(stack) == 0 and len(queue) == 0
+
+
 # way 1
-def validate_brackets(str):
-    round_brackets=0
-    curly_brackets=0
-    square_brackets=0
-    str1=list(str)
-    for i in str1:
+# def validate_brackets(str):
+#     round_brackets=0
+#     curly_brackets=0
+#     square_brackets=0
+#     str1=list(str)
+#     for i in str1:
         
-        if i=='(':
-            round_brackets+=1
+#         if i=='(':
+#             round_brackets+=1
             
-        if i==')':
-            round_brackets-=1 
+#         if i==')':
+#             round_brackets-=1 
 
-        if i=='[':
-            square_brackets+=1
-        if i==']':
-            square_brackets-=1
+#         if i=='[':
+#             square_brackets+=1
+#         if i==']':
+#             square_brackets-=1
 
-        if i=='{':
-            curly_brackets+=1
-        if i=='}':
-            curly_brackets-=1
+#         if i=='{':
+#             curly_brackets+=1
+#         if i=='}':
+#             curly_brackets-=1
     
-    if round_brackets==0 and curly_brackets==0 and square_brackets==0:
-        return True
-    else:
-        return False
+#     if round_brackets==0 and curly_brackets==0 and square_brackets==0:
+#         return True
+#     else:
+#         return False
 # way 2
-def validate_brackets_2(str):
-    stack=Stack()
-    str1=list(str)
-    str2=[]
-    for j in str1:
-        if j=='(' or j=='[' or j=='{':
-            str2.append(j)
-    # print(str2)
+# def validate_brackets_2(str):
+#     stack=Stack()
+#     str1=list(str)
+#     str2=[]
+#     for j in str1:
+#         if j=='(' or j=='[' or j=='{':
+#             str2.append(j)
+#     # print(str2)
     
-    for j in str1:
-        if j==')' or j==']' or j=='}':
-            str2.append(j)
-    # print(str2)
-    # print(str1)
+#     for j in str1:
+#         if j==')' or j==']' or j=='}':
+#             str2.append(j)
+#     # print(str2)
+#     # print(str1)
     
-    for i in str2:
-        # print(i)
-        if i =='(' or i=='[' or i=='{':
-            stack.push(i)
-        if i==')':
-            if stack.pop_specific_value('(') is None:
-                return False
-        elif i==']':
-            if stack.pop_specific_value('[') is None:
-                return False
-        elif i=='}':
-            if stack.pop_specific_value('{') is None:
-                return False
+#     for i in str2:
+#         # print(i)
+#         if i =='(' or i=='[' or i=='{':
+#             stack.push(i)
+#         if i==')':
+#             if stack.pop_specific_value('(') is None:
+#                 return False
+#         elif i==']':
+#             if stack.pop_specific_value('[') is None:
+#                 return False
+#         elif i=='}':
+#             if stack.pop_specific_value('{') is None:
+#                 return False
             
-    if stack.get_size()==0:
-        return True
-    else:
-        return False
+#     if stack.get_size()==0:
+#         return True
+#     else:
+#         return False
 
-strr="hello mr. (ahm)[a)d"
-result=validate_brackets(strr)
-print(result)
+# strr="hello mr. (ahm)[a)d"
+# result=validate_brackets(strr)
+# print(result)
--- tests/stack_queue_brackets_CC13_test/stack_queue_brackets_test.py
+++ tests/stack_queue_brackets_CC13_test/stack_queue_brackets_test.py
@@ -1,67 +1,32 @@
-from code_challenges.stack_and_queue.stack_queue_brackets_CC13_folder.stack_queue_brackets_file import validate_brackets_2
+from code_challenges.stack_and_queue.stack_queue_brackets_CC13_folder.stack_queue_brackets_file import is_balanced_parenthesis
 from code_challenges.stack_and_queue.stack_queue_brackets_CC13_folder.stack_queue_brackets_file import validate_brackets
 
 
-def test_1():
-    arr1="(ahmad[)]"
-    expected = True
-    actual = validate_brackets_2(arr1)
-    assert expected == actual
+def test_is_balanced_parenthesis_balanced():
+    assert is_balanced_parenthesis("()") == True
+    assert is_balanced_parenthesis("()()") == True
+    assert is_balanced_parenthesis("(())") == True
+    assert is_balanced_parenthesis("((()))") == True
+    assert is_balanced_parenthesis("()()()") == True
+    assert is_balanced_parenthesis("()(())") == True
 
-def test_empty():
-    arr1="ahmad"
-    expected = True
-    actual = validate_brackets_2(arr1)
-    assert expected == actual
+def test_is_balanced_parenthesis_unbalanced():
+    assert is_balanced_parenthesis("(") == False
+    assert is_balanced_parenthesis(")") == False
+    assert is_balanced_parenthesis(")(") == False
+    assert is_balanced_parenthesis("()(") == False
+    assert is_balanced_parenthesis(")()(") == False
 
-def test_2():
-    arr1="(ahmad[){]"
-    expected = False
-    actual = validate_brackets_2(arr1)
-    assert expected == actual
+def test_is_balanced_parenthesis_empty():
+    assert is_balanced_parenthesis("") == True
 
-def test_3():
-    arr1="(ah}mad[)]"
-    expected = False
-    actual = validate_brackets_2(arr1)
-    assert expected == actual
+def test_is_balanced_parenthesis_nested():
+    assert is_balanced_parenthesis("((())())") == True
+    assert is_balanced_parenthesis("((()())") == False
+    assert is_balanced_parenthesis("(((())))") == True
+    assert is_balanced_parenthesis("()((())())") == True
 
-
-# tests for way 1
-
-def test_4():
-    arr1="(ah}m{ad[)}}{{]"
-    expected = True
-    actual = validate_brackets(arr1)
-    assert expected == actual
-
-def test_5():
-    arr1="(ahmad[)]"
-    expected = True
-    actual = validate_brackets(arr1)
-    assert expected == actual
-
-def test_empty2():
-    arr1="ahmad"
-    expected = True
-    actual = validate_brackets(arr1)
-    assert expected == actual
-
-def test_6():
-    arr1="(ahmad[){]"
-    expected = False
-    actual = validate_brackets(arr1)
-    assert expected == actual
-
-
-def test_7():
-    arr1="(ah}mad[)]"
-    expected = False
-    actual = validate_brackets(arr1)
-    assert expected == actual
-
-def test_8():
-    arr1="(ah}m{ad[)}}{{]"
-    expected = True
-    actual = validate_brackets(arr1)
-    assert expected == actual
+def test_is_balanced_parenthesis_other_characters():
+    assert is_balanced_parenthesis("(abc(def)xyz)") == True
+    assert is_balanced_parenthesis("(abc(def)xyz") == False
+    assert is_balanced_parenthesis("abc(def)xyz") == True
