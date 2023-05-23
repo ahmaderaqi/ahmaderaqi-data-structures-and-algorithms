6b8d7bcb740a3468ab560967e3ab548b4ae4b651
ahmaderaqi <ahmadzaher573@gmail.com>
2023-05-23 17:56:15 +0000

    kkk

--- code_challenges/trees/binary_search_tree_folder_cc15/binary_search_tree_file.py
+++ code_challenges/trees/binary_search_tree_folder_cc15/binary_search_tree_file.py
@@ -1,78 +1,84 @@
 class Node:
-
-    def __init__(self,value):
+    def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 
-class Tree:
+class BinaryTree:
     def __init__(self):
         self.root = None
 
+    def pre_order_traversal(self):
+        result = []
+        self._pre_order_recursive(self.root, result)
+        return result
 
-tree1 = Tree()
+    def _pre_order_recursive(self, node, result):
+        if node is not None:
+            result.append(node.value)
+            self._pre_order_recursive(node.left, result)
+            self._pre_order_recursive(node.right, result)
 
-node1 = Node("10")
-tree1.root = node1
+    def in_order_traversal(self):
+        result = []
+        self._in_order_recursive(self.root, result)
+        return result
 
-node2 = Node(8)
-tree1.root.left = node2
+    def _in_order_recursive(self, node, result):
+        if node is not None:
+            self._in_order_recursive(node.left, result)
+            result.append(node.value)
+            self._in_order_recursive(node.right, result)
 
-node3 = Node(20)
-tree1.root.right = node3
+    def post_order_traversal(self):
+        result = []
+        self._post_order_recursive(self.root, result)
+        return result
 
-node4 = Node(6)
-tree1.root.left.left = node4
+    def _post_order_recursive(self, node, result):
+        if node is not None:
+            self._post_order_recursive(node.left, result)
+            self._post_order_recursive(node.right, result)
+            result.append(node.value)
 
-node5 = Node(30)
-tree1.root.left.right = node5
 
-node6 = Node(19)
-tree1.root.right.left = node6
 
-def pre_order(root):
-    if root is not None:
-        print(root.value)
-    if root.left is not None:
-        pre_order(root.left)
-    if root.right is not None:
-        pre_order(root.right)
 
-def in_order(root):
-    if root.left is not None:
-        in_order(root.left)
-    if root is not None:
-        print(root.value)
-    if root.right is not None:
-        in_order(root.right)
-    
-def post_order(root):
-    if root.left is not None:
-        in_order(root.left)
-    if root.right is not None:
-        in_order(root.right)
-    if root is not None:
-        print(root.value)
 
-def Add(root, value):
-    if value < root.value:
-        if root.left is None:
-            root.left = Node(value)
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def add(self, value):
+        if self.root is None:
+            self.root = Node(value)
         else:
-            Add(root.left, value)
-    else:
-        if root.right is None:
-            root.right = Node(value)
-        else:
-            Add(root.right, value)
+            self._add_recursive(self.root, value)
 
-def Contains(root, value):
-    if root is None:
-        return False
-    
-    if value == root.value:
-        return True
-    elif value < root.value:
-        return Contains(root.left, value)
-    else:
-        return Contains(root.right, value)
+    def _add_recursive(self, current, value):
+        if value < current.value:
+            if current.left is None:
+                current.left = Node(value)
+            else:
+                self._add_recursive(current.left, value)
+        elif value > current.value:
+            if current.right is None:
+                current.right = Node(value)
+            else:
+                self._add_recursive(current.right, value)
+
+    def contains(self, value):
+        return self._contains_recursive(self.root, value)
+
+    def _contains_recursive(self, current, value):
+        if current is None:
+            return False
+        if value == current.value:
+            return True
+        elif value < current.value:
+            return self._contains_recursive(current.left, value)
+        else:
+            return self._contains_recursive(current.right, value)
+
+
+
--- tests/binary_tree_test/binay_tree_test.py
+++ tests/binary_tree_test/binay_tree_test.py
@@ -1,55 +1,49 @@
-from code_challenges.trees.binary_search_tree_folder_cc15.binary_search_tree_file import post_order
-from code_challenges.trees.binary_search_tree_folder_cc15.binary_search_tree_file import pre_order
-from code_challenges.trees.binary_search_tree_folder_cc15.binary_search_tree_file import in_order
-
-from code_challenges.trees.binary_search_tree_folder_cc15.binary_search_tree_file import Tree
-from code_challenges.trees.binary_search_tree_folder_cc15.binary_search_tree_file import Add
-from code_challenges.trees.binary_search_tree_folder_cc15.binary_search_tree_file import Contains
-
+from code_challenges.trees.binary_search_tree_folder_cc15.binary_search_tree_file import BinarySearchTree
+from code_challenges.trees.binary_search_tree_folder_cc15.binary_search_tree_file import BinaryTree
 from code_challenges.trees.binary_search_tree_folder_cc15.binary_search_tree_file import Node
 import pytest
 
 
-@pytest.fixture
-def bst_tree():
-    root = Node(50)
-    (root, 30)
-    Add(root, 20)
-    Add(root, 40)
-    Add(root, 70)
-    Add(root, 60)
-    Add(root, 80)
-    return root
 
-def test_contains_value_existing_value():
-    root = Node(50)
-    root.left = Node(30)
-    root.right = Node(70)
-    root.left.left = Node(20)
-    root.left.right = Node(40)
-    root.right.left = Node(60)
-    root.right.right = Node(80)
-    
-    assert Contains(root, 40) == True
-    assert Contains(root, 60) == True
-    assert Contains(root, 50) == True
 
-def test_contains_value_nonexistent_value():
-    root = Node(50)
-    root.left = Node(30)
-    root.right = Node(70)
-    root.left.left = Node(20)
-    root.left.right = Node(40)
-    root.right.left = Node(60)
-    root.right.right = Node(80)
 
-    assert Contains(root, 10) == False
-    assert Contains(root, 90) == False
-    assert Contains(root, 45) == False
 
-def test_contains_value_empty_tree():
-    root = None
+def test_binary_search_tree():
+    bst = BinarySearchTree()
 
-    assert Contains(root, 10) == False
-    assert Contains(root, 20) == False
-    assert Contains(root, 30) == False
+    # Add nodes
+    bst.add(5)
+    bst.add(2)
+    bst.add(7)
+    bst.add(1)
+    bst.add(4)
+
+    # Check if values are present in the tree
+    assert bst.contains(4) is True
+    assert bst.contains(6) is False
+    assert bst.contains(2) is True
+    assert bst.contains(10) is False
+
+def test_binary_tree_traversals():
+    bt = BinaryTree()
+
+    # Create the binary tree
+    bt.root = Node(1)
+    bt.root.left = Node(2)
+    bt.root.right = Node(3)
+    bt.root.left.left = Node(4)
+    bt.root.left.right = Node(5)
+
+    # Perform pre-order traversal
+    pre_order_result = bt.pre_order_traversal()
+    assert pre_order_result == [1, 2, 4, 5, 3]
+
+    # Perform in-order traversal
+    in_order_result = bt.in_order_traversal()
+    assert in_order_result == [4, 2, 5, 1, 3]
+
+    # Perform post-order traversal
+    post_order_result = bt.post_order_traversal()
+    assert post_order_result == [4, 5, 2, 3, 1]
+if __name__ == "__main__":
+    pytest.main()
