dbeaf8a3bf0ab956e56e0862efb2d92056b9b7fc
ahmad zaher <118004544+ahmaderaqi@users.noreply.github.com>
2023-05-25 12:46:17 +0300

    Update insert_test.py

--- tests/insert_test.py
+++ tests/insert_test.py
@@ -7,14 +7,14 @@ def test_one():
     actual = insertShiftArray(arr1,10)
     assert expected == actual
 
-# def test_two():
-#     arr2=[1,2]
-#     expected = [1,5,2]
-#     actual = insertShiftArray(arr2,5)
-#     assert expected == actual
+def test_two():
+    arr2=[1,2]
+    expected = [1,5,2]
+    actual = insertShiftArray(arr2,5)
+    assert expected == actual
 
-# def test_three():
-#     arr3=[1]
-#     expected = [1,10]
-#     actual = insertShiftArray(arr3,10)
-#     assert expected == actual
+def test_three():
+    arr3=[1]
+    expected = [1,10]
+    actual = insertShiftArray(arr3,10)
+    assert expected == actual
